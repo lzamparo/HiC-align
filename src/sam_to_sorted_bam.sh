38a4f55bbc24d435d61ff8071ed48c038755c133
Lee Zamparo <zamparo@gmail.com>
2015-05-18 20:00:29 -0400

    renamed pipeline.sh, added sam_to_sorted_bam.sh

---
+++ src/sam_to_sorted_bam.sh
@@ -0,0 +1,63 @@
+! /bin/bash
+
+### try to catch a tonne of errors (h/t Michael Hoffman)
+set -o nounset -o pipefail -o errexit
+
+### grab genomic locations:
+hg=$1
+rep_prefix=$2
+rep1=$3
+rep2=$4
+min_quality=$5
+
+### convert sam to sorted bam files, filtering the poor quality reads
+
+sam_to_bam(){
+	outfile=`echo $1 | sed -e 's/.sam/.bam/g'`
+	samtools view -bSq $2 $1 > $outfile 
+}
+
+export -f sam_to_bam
+
+echo "convert sam to bam?"
+select convert in "y" "n";
+do
+	if [ "$convert" == "y" ]; then
+
+		echo "input scaled phred probability quality cutoff value"
+		read qval
+
+		cd "$rep_prefix$rep1"
+		echo "currently in "`pwd`
+		echo "using cutoff score $qval"
+		parallel -j10 --dry-run --progress --xapply sam_to_bam ::: $qval ::: `ls -1 *.sam`
+		
+
+		cd "$rep_prefix$rep2"
+		echo "currently in "`pwd`
+		parallel -j10 --dry-run --progress --xapply sam_to_bam ::: $qval ::: `ls -1 *.sam`
+	fi
+	break;
+done
+
+echo "delete sam files?"
+select rmsam in "y" "n";
+do
+	if [ "$rmsam" == "y"]; then
+		cd "$rep_prefix$rep1"
+		rm *.sam
+
+		cd "$rep_prefix$rep2"
+		rm *.sam
+	fi
+	break;
+done	
+
+sort_sam(){
+
+}
+
+echo "Sort and index bam files?"
+select sortindex in "y" "n";
+do
+	if [ "$sortindex" == "y" ]; then
