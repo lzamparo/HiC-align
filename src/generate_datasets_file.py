f46b0a31eeca178baaff258c34eab82915c3a06b
Lee Zamparo <zamparo@gmail.com>
2015-06-25 16:11:36 -0400

    added generate_datasets_file.py

---
+++ src/generate_datasets_file.py
@@ -0,0 +1,52 @@
+from mirnylib.h5dict import h5dict
+#import pandas as pd
+#from mirnylib.systemutils import setExceptionHook
+import os
+from argparse import ArgumentParser
+
+"""
+Adapted from the hiclib/examples/pipeline2015/01_makeDatasetsFile.py
+
+The goal of this file is to create a datasets.tsv file.
+
+The file has the following structure:
+Filename                                Experiment  Replicate   Genome  RestrictionEnzyme
+
+mapped-hg19/SRR3141592/chunk0001.hdf5   K562        R1          hg19    HindIII
+mapped-hg19/SRR3141592/chunk0002.hdf5   K562        R1          hg19    HindIII
+mapped-hg19/SRR3141593/chunk0001.hdf5   K562        R1          hg19    HindIII
+mapped-hg19/SRR3141593/chunk0002.hdf5   K562        R1          hg19    HindIII
+mapped-hg19/SRR3141594/chunk0001.hdf5   K565        R2          hg19    HindIII
+
+"""
+
+parser = ArgumentParser()
+parser.add_argument("-b", "--basedirectory", help="the base directory for the data in the project")
+parser.add_argument("-r", "--runsfile", help="the runs.tsv file")
+parser.add_argument("-d", "--datafile", help="the dataset file to be output (default name is datasets.tsv, same dir as runs file)")
+args = parser.parse_args()
+
+# open and parse the runs file
+runs_file = open(os.path.join(args.b,args.r),"r")
+runs = [run.split() for run in runs_file.readlines() if not run.startswith("#")]
+runs_file.close()
+
+# process each record in the runs file, write out to the data sets file
+datasets_file = open(os.path.join(args.b,args.d),"w")
+
+for run in runs:
+    input_dir, experiment, replicate, genome, restriction_enzyme = run
+    filenames = [j for j in os.listdir(os.path.join(args.b,input_dir)) if j.endswith(".hdf5") ]
+    for fname in filenames:
+        try:
+            mydict = h5dict(os.path.join(args.b,input_dir,fname),'r')
+        except:
+            pass
+        if "strands1" not in mydict:
+            raise
+        if len(mydict.get_dataset("strands1")) < 10000:
+            raise
+        datasets_file.write("{0} {1} {2} {3} {4}\n".format(os.path.join(input_dir,fname),experiment,
+                                                    replicate, genome, restriction_enzyme))
+
+datasets_file.close()
