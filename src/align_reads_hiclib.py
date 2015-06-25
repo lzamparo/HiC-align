4552f0eead824e554e1d38aa1f77dd9f938975b0
Lee Zamparo <zamparo@gmail.com>
2015-06-25 16:57:16 -0400

    changed assignment of scratch dir in align_reads...

--- src/align_reads_hiclib.py
+++ src/align_reads_hiclib.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
 	tmp_dir = "/home/zamparol/scratch"  
 
 	# they need some object to represent hg19
-	genome_db = genome.Genome(args.hg19, readChrms=["#", "X", "Y"], cacheDir="tmp_dir")
+	genome_db = genome.Genome(args.hg19, readChrms=["#", "X", "Y"], cacheDir=tmp_dir)
 	genome_name = genome_db.folderName  # automatically infer genome name from the folder name. Name is used for naming folders ("mapped-hg19", etc)
 
 	seq_skip_start = 2  # skip first 2 bp of the read, if you want
@@ -137,20 +137,3 @@ if __name__ == "__main__":
 
 	# map the reads in parallel:
 	Parallel(n_jobs=4)(delayed(map_reads)(p,q,outf) for p,q,outf in zip(first_ends,second_ends,outfiles))
-	
-
-
-# This is a current version of the pipeline which we use for processing many Hi-C datasets. This version is for .sra files or fastq.gz files split by two sides.
-
-# Usage:
-
-# 1. place .sra files in the fastq folder
-# 2. edit 00_mapData.py. Specify genome and paths to bowtie, and other arguments
-# 3. provide runs.tsv file as described in 01_makeDatasetsFile.py
-# 4. possibly edit 02_mergeDatasets.py to specify resolutions (especially when working with smaller genomes)
-# 5. run 02_mergeDatasets.py   -  it will do several things.
-#     -- merge (if necessary) different .hdf5 files corresponding to one replica of one datasets
-#     -- do fragment-level filtering of merged files
-#     -- save heatmaps at different resolutions
-#     -- merge files from different replicas of the same experiment
-#     -- save heatmaps
