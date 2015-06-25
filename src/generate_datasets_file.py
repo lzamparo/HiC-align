dd39309c82d499d02f99c5abcdb3cff431523da6
Lee Zamparo <zamparo@gmail.com>
2015-06-25 16:27:34 -0400

    fixed namespace lookup error in parsed args

--- src/generate_datasets_file.py
+++ src/generate_datasets_file.py
@@ -21,25 +21,25 @@ mapped-hg19/SRR3141594/chunk0001.hdf5   K565        R2          hg19    HindIII
 """
 
 parser = ArgumentParser()
-parser.add_argument("-b", "--basedirectory", help="the base directory for the data in the project")
+parser.add_argument("-b", "--basedir", help="the base directory for the data in the project")
 parser.add_argument("-r", "--runsfile", help="the runs.tsv file")
 parser.add_argument("-d", "--datafile", help="the dataset file to be output (default name is datasets.tsv, same dir as runs file)")
 args = parser.parse_args()
 
 # open and parse the runs file
-runs_file = open(os.path.join(args.b,args.r),"r")
+runs_file = open(os.path.join(args.basedir,args.runsfile),"r")
 runs = [run.split() for run in runs_file.readlines() if not run.startswith("#")]
 runs_file.close()
 
 # process each record in the runs file, write out to the data sets file
-datasets_file = open(os.path.join(args.b,args.d),"w")
+datasets_file = open(os.path.join(args.basedir,args.datafile),"w")
 
 for run in runs:
     input_dir, experiment, replicate, genome, restriction_enzyme = run
-    filenames = [j for j in os.listdir(os.path.join(args.b,input_dir)) if j.endswith(".hdf5") ]
+    filenames = [j for j in os.listdir(os.path.join(args.basedir,input_dir)) if j.endswith(".hdf5") ]
     for fname in filenames:
         try:
-            mydict = h5dict(os.path.join(args.b,input_dir,fname),'r')
+            mydict = h5dict(os.path.join(args.basedir,input_dir,fname),'r')
         except:
             pass
         if "strands1" not in mydict:
